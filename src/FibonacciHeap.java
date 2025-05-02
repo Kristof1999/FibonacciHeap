d3030f51f607d8fa8a6e44975c8705dd744f729c
Kristof <nagykristof958@gmail.com>
2025-05-02 10:36:20 +0200

    insert and getMin for FibonacciHeap

--- src/FibonacciHeap.java
+++ src/FibonacciHeap.java
@@ -1,3 +1,45 @@
-public class FibonacciHeap {
+import java.util.Optional;
+import java.util.OptionalInt;
 
+// src:
+// - https://en.wikipedia.org/wiki/Fibonacci_heap
+// - https://en.wikipedia.org/wiki/Doubly_linked_list
+// - MICHAEL L. FREDMAN and ROBERT ENDRE TARJAN: Fibonacci Heaps and Their Uses in Improved Network
+//   Optimization Algorithms
+public class FibonacciHeap {
+    private Optional<Item> minItem = Optional.empty();
+
+    public FibonacciHeap(int[] items) {
+        if (items.length == 0)
+            return;
+
+        for (var item : items) {
+            insert(item);
+        }
+    }
+
+    public OptionalInt getMin() {
+        if(minItem.isEmpty())
+            return OptionalInt.empty();
+        else
+            return OptionalInt.of(minItem.get().value);
+    }
+
+    public void insert(int itemValue) {
+        var item = new Item();
+        item.value = itemValue;
+        if (minItem.isEmpty()) {
+            minItem = Optional.of(item);
+            return;
+        }
+
+        item.right = minItem.get().right;
+        item.left = minItem.get();
+        minItem.get().right.left = item;
+        minItem.get().right = item;
+
+        if(minItem.get().value > item.value) {
+            minItem = Optional.of(item);
+        }
+    }
 }
--- src/FibonacciHeapTest.java
+++ src/FibonacciHeapTest.java
@@ -1,5 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.OptionalInt;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciHeapTest {
 
+    private int[] input = new int[] {100, 19, 36, 17, 3, 25, 1, 2, 7};
+
+    @Test
+    public void constructionTest() {
+        var heap = new FibonacciHeap(input);
+    }
+
+    @Test
+    public void minTest() {
+        var heap = new FibonacciHeap(input);
+        assertEquals(1, heap.getMin().getAsInt());
+    }
+
+    @Test
+    public void minEmptyTest() {
+        var heap = new FibonacciHeap(new int[0]);
+        assertEquals(OptionalInt.empty(), heap.getMin());
+    }
 }
